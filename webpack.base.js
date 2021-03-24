c9e98ac8cce37e6431d5570bcaf3524ed4797654
hakimmaaouia <hakimmaaouia@gmail.com>
2021-03-24 22:21:11 +0100

    fix config

--- webpack.base.js
+++ webpack.base.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
@@ -65,71 +64,3 @@ module.exports = {
     ignored: /node_modules/,
   },
 };
-=======
-const { resolve } = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-
-module.exports = {
-  devServer: {
-    contentBase: resolve(__dirname, "src"),
-    historyApiFallback: true,
-  },
-
-  entry: {
-    background: resolve(__dirname, "src/background.ts"),
-    inject: resolve(__dirname, "src/inject.ts"),
-
-    popup: resolve(__dirname, "src/popup/index.tsx"),
-    options: resolve(__dirname, "src/options/index.tsx"),
-    foreground: resolve(__dirname, "src/foreground/index.tsx"),
-  },
-
-  output: {
-    filename: "[name].bundle.js",
-    path: resolve(__dirname, "dist"),
-  },
-
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.tsx?$/,
-        use: "ts-loader",
-      },
-    ],
-  },
-
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "popup.html",
-      template: "src/popup/index.html",
-      chunks: ["popup"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "options.html",
-      template: "src/options/index.html",
-      chunks: ["options"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "foreground.html",
-      template: "src/foreground/index.html",
-      chunks: ["foreground"],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: "src/manifest.json", to: "[name].[ext]" },
-        { from: "src/assets/*.png", to: "[name].[ext]" },
-      ],
-    }),
-  ],
-
-  watchOptions: {
-    ignored: /node_modules/,
-  },
-};
->>>>>>> d4f99f368549b5872f25e4a4e3c37d168813ece2
